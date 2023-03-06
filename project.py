ef2771a6bfa4748f18e0f66cd4a019938b1b32a5
Sandra Sanchez <sandra.sanchez.paez@uni-potsdam.de>
2023-03-06 14:30:43 +0100

    Add mlp neural netwrok baseline

--- project.py
+++ project.py
@@ -1,12 +1,18 @@
 # Sandra Sanchez
 # NLP Eval project
 
+import numpy as np
+
 from pathlib import Path
 
 from conllu import parse
+from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.tag import hmm, crf, perceptron
 from sklearn import metrics
 from sklearn.dummy import DummyClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.svm import SVC
+
 
 
 def read_datasets(corpus_dirname, train_filename, dev_filename,  test_filename):
@@ -37,7 +43,7 @@ def tag_datasets(train_set, val_set, test_set):
 
 
 def get_tokens_from_datasets(train_set, val_set, test_set):
-    tokens_train = [[(word['form'], '') for word in sent] for sent in train_set]
+    tokens_train = [[word['form'] for word in sent] for sent in train_set]
     tokens_val = [[word['form'] for word in sent] for sent in val_set]
     tokens_test = [[word['form'] for word in sent] for sent in test_set]
 
@@ -83,16 +89,37 @@ def get_gold_labels(gold_labels_train, gold_labels_val, gold_labels_test):
     return gold_train, gold_val, gold_test
 
 
-def train_baseline(tokens_train, gold_train, tokens_val, gold_val):
-    classifier = DummyClassifier(strategy='most_frequent')
-    tokens_train = [token for sentence  in tokens_train for token in sentence]
-    tokens_val = [token for sentence  in tokens_val for token in sentence]
-    # print(len(tokens_train), len(gold_train))  # 4274 48655
-    # tokens_train = [t for sentence in tokens_train for t in sentence]  # [6644, 572]
-    classifier.fit(tokens_train, gold_train)
-    binary_balanc_predictions = classifier.predict(tokens_val)
-    binary_balanc_probs = classifier.predict_proba(tokens_val)
-    print(f'Accuracy on the val set with dummy classifier: {classifier.score(tokens_val, gold_val)}')
+def transform_raw_data_into_matrix(train_sentences_list, labels_list, dev_sents_list):
+    assert len(train_sentences_list) == len(labels_list)
+    words = [token for sentence in train_sentences_list for token in sentence]
+    labels = [token for sentence in labels_list for token in sentence]
+    words_dev = [token for sentence in dev_sents_list for token in sentence]
+    # print(sentences)
+    y = np.array(labels)
+    vectorizer = TfidfVectorizer()
+    X_train = vectorizer.fit_transform(words)
+    X_dev = vectorizer.transform(words_dev)
+    return X_train, y, X_dev
+
+
+def train_baseline(tokens_train, gold_labels_train, gold_train, tokens_val, gold_val, strategy):
+    if strategy == 'most_frequent':
+        classifier = DummyClassifier(strategy='most_frequent')
+        tokens_train = [token for sentence in tokens_train for token in sentence]
+        tokens_val = [token for sentence in tokens_val for token in sentence]
+        # print(len(tokens_train), len(gold_train))  # 4274 48655
+        # tokens_train = [t for sentence in tokens_train for t in sentence]  # [6644, 572]
+        classifier.fit(tokens_train, gold_train)
+        multiclass_predictions = classifier.predict(tokens_val)
+        multiclass_imbalanc_probs = classifier.predict_proba(tokens_val)
+        print(f'Accuracy on the val set with strategy {strategy}: {classifier.score(tokens_val, gold_val)}')
+    elif strategy == 'mlp':
+        classifier = MLPClassifier(verbose=True)
+        X, y, X_val = transform_raw_data_into_matrix(tokens_train, gold_labels_train, tokens_val)
+        classifier.fit(X, y)
+        multiclass_predictions = classifier.predict(X_val)
+        multiclass_imbalanc_probs = classifier.predict_proba(X_val)
+        print(f'Accuracy on the val set with strategy {strategy}: {classifier.score(X_val, gold_val)}')
 
 
 train_data, val_data, test_data = read_datasets('UD_English-Atis-master/', 'en_atis-ud-train.conllu', 'en_atis-ud-dev.conllu', 'en_atis-ud-test.conllu')
@@ -106,7 +133,9 @@ model_outputs, tagger, accuracy, predicted_labels = train_model(tagged_train, to
 gold_train, gold_val, gold_test = get_gold_labels(gold_labels_train, gold_labels_val, gold_labels_test)
 print(metrics.classification_report(gold_val, predicted_labels, zero_division=0))
 # print(len(gold_val), len(predicted_labels))  # 572 6644
-train_baseline(tokens_train, gold_train, tokens_val, gold_val)
+# print(tokens_train[:3], gold_labels_train[:3])
+train_baseline(tokens_train, gold_labels_train, gold_train, tokens_val, gold_val, 'most_frequent')  # 0.23344370860927152
+train_baseline(tokens_train, gold_labels_train, gold_train, tokens_val, gold_val, 'mlp')  # Accuracy on the val set with strategy mlp: 0.9262492474413004
 
 ##########################
 
